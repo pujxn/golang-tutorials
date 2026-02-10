d05f1259a945d478986eaf311b046321e7939835
puparikh <puparikh@iu.edu>
2026-02-10 14:10:05 +0530

    Added functionality for greetings for multiple people

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -16,6 +16,20 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+func Hellos(names []string) (map[string]string, error) {
+
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[message] = message
+	}
+	return messages, nil
+}
+
 func randomFormat() string {
 	formats := []string{"Hi, %v. Welcome!", "Hey there, %v! Howdy?", "What's up, %v? All good?"}
 	return formats[rand.Intn(len(formats))]
--- hello/hello.go
+++ hello/hello.go
@@ -17,4 +17,12 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(message)
+
+	messages, err := greetings.Hellos([]string{"John", "Jacob", "Tony"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, message := range messages {
+		fmt.Println(message)
+	}
 }
